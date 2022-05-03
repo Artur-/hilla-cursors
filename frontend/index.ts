ec6b0f99f2c1dce351a988c4253f8642bda3d56e
Artur Signell <artur@vaadin.com>
2022-05-03 17:02:49 +0300

    Github corner

--- frontend/index.ts
+++ frontend/index.ts
@@ -2,6 +2,7 @@ import { Router } from '@vaadin/router';
 import { routes } from './routes';
 import { appStore } from './stores/app-store';
 import './cursor-tracker'
+import 'github-corner'
 export const router = new Router(document.querySelector('#outlet'));
 
 router.setRoutes(routes);
