88d488ada2be412c267519fde4755a2fa54a0941
Artur Signell <artur@vaadin.com>
2022-05-27 16:06:38 +0300

    Handle reconnect

--- frontend/cursor-tracker.ts
+++ frontend/cursor-tracker.ts
@@ -1,8 +1,9 @@
-import { Subscription } from '@hilla/frontend';
+import { Subscription, State } from '@hilla/frontend';
 import { html, LitElement } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
 import { throttle } from 'throttle-debounce';
 import './cursor-indicator';
+import client from './generated/connect-client.default';
 import { CursorTracker } from './generated/endpoints';
 import Cursor from './generated/org/vaadin/artur/hillacursor/endpoints/Cursor';
 
@@ -11,25 +12,36 @@ export class TheView extends LitElement {
   cursorX: number = -1;
   cursorY: number = -1;
 
+  id: string = Math.random().toString(36).substring(2, 9);
+
   @state()
   cursors: Cursor[] = [];
   cursorId: string | undefined;
   sendTimer: any;
   sub?: Subscription<Cursor>;
+  @state()
+  updatesPaused = false;
 
   async connectedCallback() {
     super.connectedCallback();
 
-    this.cursorId = await CursorTracker.join(window.navigator.userAgent);
-    this.cursors = await CursorTracker.getCursors(this.cursorId);
-    this.cursors = this.cursors.filter(cursor => cursor.id !== this.cursorId);
-    console.debug('Got cursors', this.cursors);
-    const maxTimestamp = Math.max(...this.cursors.map((c) => c.timestamp));
-    this.sub = CursorTracker.subscribe(this.cursorId, maxTimestamp).onNext((value) => {
-      console.debug('Got ', value);
-      if (value.id !== this.cursorId) {
-        this.cursors = [...this.cursors.filter((cursor) => cursor.id !== value.id), value];
-      }
+    client.fluxConnection.addEventListener('state-changed', (e: CustomEvent) => {
+      this.updatesPaused = !e.detail.active;
+    });
+
+    keepAlive(async () => {
+      this.cursorId = await CursorTracker.join(this.id);
+      this.cursors = await CursorTracker.getCursors(this.cursorId);
+      const maxTimestamp = Math.max(...this.cursors.map((c) => c.timestamp));
+      this.sub = CursorTracker.subscribe(this.cursorId, maxTimestamp).onNext((value: Cursor) => {
+        console.debug('Got ', value);
+        if (value.id !== this.cursorId) {
+          this.cursors = [...this.cursors.filter((cursor) => cursor.id !== value.id)];
+          if (value.color !== 'DELETE') {
+            this.cursors = [...this.cursors, value];
+          }
+        }
+      });
     });
 
     const sendCursor = throttle(200, (cursorX: number, cursorY: number) => {
@@ -38,15 +50,37 @@ export class TheView extends LitElement {
     });
 
     document.addEventListener('mousemove', (e: MouseEvent) => {
-      sendCursor(e.clientX, e.clientY);
+      if (this.cursorId) {
+        sendCursor(e.clientX, e.clientY);
+      }
     });
   }
 
   render() {
-    return html`<button @click=${() => this.close()}>close</button> ${this.cursors.map((cursor) => html`<cursor-indicator .cursor=${cursor}></cursor-indicator>`)} `;
+    return html`<button @click=${() => this.close()}>close</button>
+
+      ${this.updatesPaused
+        ? html`<cursor-indicator
+            .cursor=${{
+              name: 'Server connection lost',
+              y: 200,
+              x: 200,
+            }}
+          ></cursor-indicator>`
+        : this.cursors.map((cursor) => html`<cursor-indicator .cursor=${cursor}></cursor-indicator>`)} `;
   }
 
   close() {
     this.sub?.cancel();
   }
+}
+function keepAlive(onReconnect: () => void) {
+  if (client.fluxConnection.state === State.ACTIVE) {
+    onReconnect();
+  }
+  client.fluxConnection.addEventListener('state-changed', (e: CustomEvent) => {
+    if (e.detail.active) {
+      onReconnect();
+    }
+  });
 }
